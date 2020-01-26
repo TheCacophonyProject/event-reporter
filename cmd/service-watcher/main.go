0a13e7bef2797a49b15b04aa74a9fe327ae34e81
cam <cameron.ryan.pears@gmail.com>
2020-01-27 10:18:59 +1300

    Added service-watcher

---
+++ cmd/service-watcher/main.go
@@ -0,0 +1,164 @@
+/*
+event-reporter - report events to the Cacophony Project API.
+Copyright (C) 2020, The Cacophony Project
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+
+	systemdbus "github.com/coreos/go-systemd/dbus"
+	"github.com/godbus/dbus"
+)
+
+const (
+	minTimeBetweenReports = time.Minute
+)
+
+type LogReport struct {
+	Logs        *[]string
+	Time        time.Time
+	UnitName    string
+	ActiveState string
+}
+
+type LogsRaw struct {
+	Unit              string `json:"UNIT"`
+	SystemdUnit       string `json:"_SYSTEMD_UNIT"`
+	RealtimeTimestamp string `json:"__REALTIME_TIMESTAMP"`
+	Message           string `json:"MESSAGE"`
+}
+
+func main() {
+	err := runMain()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func runMain() error {
+	log.SetFlags(0) // Removes default timestamp flag
+
+	conn, err := systemdbus.New()
+	if err != nil {
+		log.Println("failed to connect to dbus")
+		return err
+	}
+
+	defer conn.Close()
+
+	if err := conn.Subscribe(); err != nil {
+		log.Println("failed to subscribe to the dbus")
+		return err
+	}
+
+	updateCh := make(chan *systemdbus.PropertiesUpdate, 256)
+	errCh := make(chan error, 256)
+	conn.SetPropertiesSubscriber(updateCh, errCh)
+
+	lastUnitReportTimes := map[string]time.Time{}
+
+	for {
+		select {
+		case update := <-updateCh:
+			ts := time.Now()
+			activeState := strings.Trim(update.Changed["ActiveState"].String(), "\"")
+			unitName := update.UnitName
+			// Only process states we are interested in
+			if !inActiveStates(activeState) {
+				break
+			}
+			if t, ok := lastUnitReportTimes[unitName]; ok && time.Now().Sub(t) < minTimeBetweenReports {
+				log.Println("reporting too often")
+				break
+			}
+
+			log.Println("unitname:", unitName)
+			log.Println("activeState:", activeState)
+
+			rawLogs, err := getLogs(update.UnitName, 20)
+			if err != nil {
+				return err
+			}
+			logReport, err := json.Marshal(&LogReport{
+				UnitName:    unitName,
+				Logs:        rawLogs,
+				ActiveState: activeState,
+			})
+			if err != nil {
+				return err
+			}
+
+			conn, err := dbus.SystemBus()
+			if err != nil {
+				return err
+			}
+			obj := conn.Object("org.cacophony.Events", "/org/cacophony/Events")
+			if err := obj.Call("org.cacophony.Events.Queue", 0, logReport, ts.UnixNano()).Err; err != nil {
+				return err
+			}
+			lastUnitReportTimes[unitName] = time.Now()
+
+		case err := <-errCh:
+			log.Println("err:", err)
+		}
+	}
+}
+
+func getLogs(unitName string, number int) (*[]string, error) {
+	cmd := exec.Command(
+		"journalctl",
+		"-u", unitName,
+		"--output=json",
+		"-n", strconv.Itoa(number))
+	out, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+	strLogs := strings.Split(string(out), "\n")
+	logs := []string{}
+
+	for _, strlog := range strLogs {
+		var rawLog LogsRaw
+		json.Unmarshal([]byte(strlog), &rawLog)
+		// Only get logs from this session
+		if rawLog.SystemdUnit == "init.scope" && strings.Contains(rawLog.Message, "Started") {
+			logs = []string{}
+		}
+		logs = append(logs, rawLog.Message)
+	}
+	for _, l := range logs {
+		log.Println(l)
+	}
+	return &logs, nil
+}
+
+func inActiveStates(state string) bool {
+	switch state {
+	case
+		//"active",			// triggered when service exec is started
+		"activating", // If service is set to restart this is called after the service exec exits
+		"failed":     // If service is set to restart the service won't 'fail'.
+		return true
+	}
+	return false
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/TheCacophonyProject/modemd v0.0.0-20190605010435-ae5b0f2eb760
 	github.com/alexflint/go-arg v1.1.0
 	github.com/boltdb/bolt v1.3.1
+	github.com/coreos/go-systemd v0.0.0-20190321100706-95778dfbb74e
 	github.com/godbus/dbus v0.0.0-20181101234600-2ff6f7ffd60f
 	github.com/stretchr/testify v1.3.0
 	golang.org/x/sys v0.0.0-20190318195719-6c81ef8f67ca // indirect
