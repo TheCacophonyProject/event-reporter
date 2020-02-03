78a40cf351bed2a91cec0fdf6e7bd4e8c17122ec
cam <cameron.ryan.pears@gmail.com>
2020-02-04 12:47:37 +1300

    restart service after install

--- _release/postinstall.sh
+++ _release/postinstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 systemctl daemon-reload
+
 systemctl enable event-reporter.service
+systemctl restart event-reporter.service
+
 systemctl enable service-watcher.service
+systemctl restart service-watcher.service
